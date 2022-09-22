b736f70dfb31b3ab62ef82adc17dee5bb14a657e
Leonardus <LBotes@yellowtail.nl>
2022-09-22 17:30:37 +0200

    added firebase + realtime database + hosting + github workflow

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { FirebaseConfig } from './app/configs/firebase';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -7,6 +8,14 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const app = initializeApp(FirebaseConfig);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
